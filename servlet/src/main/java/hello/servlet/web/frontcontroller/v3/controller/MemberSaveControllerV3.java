2b491a43de9b3abf912891820d56a4b2ac9e8289
ghkdwlgns612 <ghkdwlgns612@naver.com>
2021-07-18 19:17:02 +0900

    front-controller

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import org.springframework.ui.Model;
+
+import java.util.Map;
+
+public class MemberSaveControllerV3 implements ControllerV3 {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public ModelView process(Map<String, String> paraMap) {
+        String username = paraMap.get("username");
+        int age = Integer.parseInt(paraMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        ModelView view = new ModelView("save-result");
+        view.getModel().put("member",member);
+        return view;
+    }
+}
